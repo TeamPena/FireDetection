cf71eb5f95d15acbcd3d8208a9ae739e8216b075
nexusofdorne <kliffordkho@gmail.com>
2016-02-25 14:20:47 +0800

    pictures

--- FireDetection/Source.cpp
+++ FireDetection/Source.cpp
@@ -70,63 +70,36 @@ void storeTrainingData(){
 	}
 }
 
-bool YCbCr(int r, int g, int b)
+void YCbCrThreshold()
 {
-			double y = 16 + r *  65.481 + g * 128.553 + b *24.996;
-			double cB = 128 + r * -37.797 - g * 74.203 + b *
-				112.0;
-
-			double cR = 128 + r * 112.00 + g * -93.7864 + b *
-				-18.214;
-
-			if (y >= cR >= cB){
-				return true;
-			}
-			else {
-				return false;
-			}
-}
-
-// subtract the background to get only the desired pixel for training
-void processTrainingImages(){
-	bool ycbcr;
 	for (int x = 0; x < processedImage.size(); x++){
-		for (int i = 0; i < frame.rows; i++)
+		for (int i = 0; i < processedImage[x].rows; i++)
 		{
-			for (int j = 0; j<frame.cols; j++)
+			for (int j = 0; j < processedImage[x].cols; j++)
 			{
 				bool isFire = false;
 
-				int b = trainingImage[x].at<cv::Vec3b>(i, j)[0];
-				int g = trainingImage[x].at<cv::Vec3b>(i, j)[1];
-				int r = trainingImage[x].at<cv::Vec3b>(i, j)[2];
+				double bT = processedImage[x].at<cv::Vec3b>(i, j)[0]; // B		
+				double gT = processedImage[x].at<cv::Vec3b>(i, j)[1]; // G
+				double rT = processedImage[x].at<cv::Vec3b>(i, j)[2]; // R
 
+				double b = bT, g = gT, r = rT;
 
+				double y = 16 + r *  65.481 + g * 128.553 + b *24.996;
+				double cB = 128 + r * -37.797 - g * 74.203 + b *
+					112.0;
 
-				//find max value and check if it is red	
-				if ((r > g && g > b))
-				{
+				double cR = 128 + r * 112.00 + g * -93.7864 + b *
+					-18.214;
 
-					//check if red is over a threshold
-					if (r > rgbThreshold.red && g > rgbThreshold.green && b < rgbThreshold.blue)
-					{
-						isFire = true;
-					}
-					ycbcr = YCbCr(r, g, b);
-					if (ycbcr && isFire){
-						isFire = true;
-					}
-					else {
-						isFire = false;
-					}
-				}
+				isFire = (y >= cR >= cB);
+
 
 				if (isFire)
 				{
-					processedImage[x].at<cv::Vec3b>(i, j)[0] = b;
-					processedImage[x].at<cv::Vec3b>(i, j)[1] = g;
-					processedImage[x].at<cv::Vec3b>(i, j)[2] = r;
-
+					processedImage[x].at<cv::Vec3b>(i, j)[0] = bT;
+					processedImage[x].at<cv::Vec3b>(i, j)[1] = gT;
+					processedImage[x].at<cv::Vec3b>(i, j)[2] = rT;
 				}
 				else
 				{
@@ -139,18 +112,68 @@ void processTrainingImages(){
 	}
 }
 
+void redThreshold(){
+	for (int x = 0; x < processedImage.size(); x++){
+		for (int i = 0; i < trainingImage[x].rows; i++)
+		{
+			for (int j = 0; j<trainingImage[x].cols; j++)
+			{
+				bool isFire = false;
+
+				int b = trainingImage[x].at<cv::Vec3b>(i, j)[0];
+				int g = trainingImage[x].at<cv::Vec3b>(i, j)[1];
+				int r = trainingImage[x].at<cv::Vec3b>(i, j)[2];
+
+				if ((r > g && g > b))
+				{
+					//check if red is over a threshold
+					if (r > rgbThreshold.red && g > rgbThreshold.green && b < rgbThreshold.blue)
+					{
+						isFire = true;
+					}
+				}
+
+				if (isFire)
+				{
+					processedImage[x].at<cv::Vec3b>(i, j)[0] = b;
+					processedImage[x].at<cv::Vec3b>(i, j)[1] = g;
+					processedImage[x].at<cv::Vec3b>(i, j)[2] = r;
+
+				}
+			}
+		}
+	}
+}
+
+// subtract the background to get only the desired pixel for training
+void processTrainingImages(){
+	cout << "proccesed Image: " << processedImage.size() << endl;
+	cout << "training IMage:" << trainingImage.size() << endl;
+	cout << "training Image row: " << trainingImage[0].rows << endl;
+	cout << "training image column: " << trainingImage[0].cols << endl;
+	redThreshold();
+	YCbCrThreshold();
+	
+}
+
 void openTrainingImages(){
 	vector<cv::String> fn;
-	glob("image/*.png", fn, false);
-	int count = fn.size();
-	cout << count << endl;
+	//cv::String  folder = "C:\\Thesis\\image\\positive";
+    cv:String folder = "image\\";
+	glob(folder, fn, false);
+	size_t count = fn.size();
 	for (size_t i = 0; i<fn.size(); i++)
 	{
-		cout << "hi" << endl;
-		Mat m = imread(fn[i]);
-		
+		cout << fn[i] << endl;
+		Mat m = imread(fn[i]);		
 		trainingImage.push_back(m);
+		processedImage.push_back(Mat());
+		processedImage[i] = cv::Mat(trainingImage[i].size().height, trainingImage[i].size().width, CV_8UC3);
 	}
+}
+
+void train(){
+
 }
 
 int main()
@@ -162,12 +185,13 @@ int main()
 	cout << "greens: " << rgbThreshold.blue << endl;
 
 	openTrainingImages();
-
-	//for (int i = 0; i<trainingImage.size(); i++){
-		//imshow("hsv", trainingImage[i]);
+	processTrainingImages();
+	for (int i = 0; i<trainingImage.size(); i++){
+		imshow("training Image", trainingImage[i]);
+		imshow("processed Image", processedImage[i]);
 		
-		//waitKey(0); //Note: wait for user input for every image
-	//}
+		waitKey(0); //Note: wait for user input for every image
+	}
 	return 0;
 
 }
